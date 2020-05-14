26073ae7c03dc1cf54c330f288da33c4eaee75c7
dathochoi <dathochoi01@yahoo.com.vn>
2020-05-14 16:45:54 +0700

    add all files

---
+++ firebase/public/1.js
@@ -0,0 +1,27 @@
+﻿ $(function(){
+   new WOW().init();
+    khoimonan = $('.nhieumon').isotope({
+        // options
+        itemSelector: '.motmon',
+        layoutMode: 'masonry'
+      });
+      khoimonan.imagesLoaded().progress( function() {
+        khoimonan.isotope('layout');
+      });
+      $('.tieudect a').click( function(){
+          dulieu =$(this).data('monan');
+          khoimonan.isotope({ filter: dulieu });
+          return false;
+        });
+     
+
+        //xu li responsive bang jquery
+        //tich ra so div ten .motmon
+        // soluongmon = $('.motmon').length;
+        
+        // docao = soluongmon * $('.motmon').height();
+        // console.log(docao);
+        // $('.noidungct').css({'height':docao})
+
+})  
+ 
